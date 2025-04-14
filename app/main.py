2a6641d8caddff7302180a72795cce40989d12b6
gagik <gagik.abovyan>
2025-04-14 22:35:17 +0400

    change token

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ from aiogram.filters import Command
 from aiogram.types import Message
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
-TOKEN = "7595011311:AAFRmsvGrXDPboyipgf7tLuWPBKShje6gSo"
+TOKEN = "7595011311:AAF0827OTNqQtyjAILUZvtjKKQ5ILcTCWk4"
 
 bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 dp = Dispatcher()
