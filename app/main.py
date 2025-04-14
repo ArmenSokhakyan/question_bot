4d90492a6b511f92d1af139f4e27f15b21ff1d4b
gagik <gagik.abovyan>
2025-04-14 21:49:46 +0400

    change loges

--- app/main.py
+++ app/main.py
@@ -217,6 +217,6 @@ async def main() -> None:
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    logging.basicConfig(level=logging.ERROR, stream=sys.stdout)
     asyncio.run(main())
 
